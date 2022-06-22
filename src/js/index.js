bd10a3119163c1a943bf58d00a5daaebda49af13
Mariia Kucheieva <mary.kucheeva@gmail.com>
2022-06-22 21:27:55 +0200

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+
+
+document.getElementById('map').addEventListener('load', function(){
+    // Will get called after embed element was loaded
+    svgPanZoom(document.getElementById('map'), {
+        zoomEnabled: true,
+        controlIconsEnabled: true
+    });
+});
+
+
+
