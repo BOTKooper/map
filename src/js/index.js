82c428a2ca56adbffc37d77668d4090ae025e3fe
Alex Ozerov <botkooper@gmail.com>
2022-06-22 22:52:59 +0300

    upd

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,25 @@
-
-
-document.getElementById('map').addEventListener('load', function(){
+document.getElementById('map').addEventListener('load', async function(){
     // Will get called after embed element was loaded
     svgPanZoom(document.getElementById('map'), {
         zoomEnabled: true,
         controlIconsEnabled: true
     });
+    const p = new Promise((r) => setTimeout(() => r(), 1000));
+    await p;
+    addOnClickToMarkers('map');
 });
 
+
+
+function addOnClickToMarkers(objectId) {
+    const markers = document.querySelectorAll("g#markers > path")
+    markers.forEach((marker) => {
+        const name = marker.getAttribute('data-name');
+        onClick = () => {
+            // fill country with new color
+        }
+        marker.onClick = onClick;
+    })
+}
 
 
